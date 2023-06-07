4eaf865c61890485f1392344bf8134951026902e
Naom19 <naosan25@protonmail.com>
2023-06-07 04:14:27 -0600

    style: colors & opacity

--- src/app.svelte
+++ src/app.svelte
@@ -15,5 +15,40 @@
 
     let pending: number;
     $: pending = todos.filter(task => !task.done).length;
-    
+
 </script>
+
+<h1>Task Master</h1>
+
+{#each todos as todo (todo.text)}
+    <div class:done = {todo.done}>
+        <input type="checkbox"
+         bind:checked={todo.done} />
+        <input placeholder="Add a new to do..." 
+         bind:value={todo.text} />
+
+    </div>
+    
+{/each}
+
+<p>{pending} pending</p>
+
+<button on:click={add}>Add</button>
+
+<button on:click={clear}>Clear completed</button>
+
+<style>
+    h1 {
+        background-color: #22267b;
+        color: #ffffff;
+    }
+    
+    .done {
+        opacity: 0.3;
+    }
+
+    button {
+        background-color: #17b794;
+        color: #01005e;
+    }
+</style>
