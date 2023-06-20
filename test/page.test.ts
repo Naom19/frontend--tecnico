c10ea3a2c773bc057089ec700a7fe8d117017dba
Naom19 <naosan25@protonmail.com>
2023-06-20 13:29:28 -0600

    test: mocking add function

--- test/page.test.ts
+++ test/page.test.ts
@@ -1,34 +1,22 @@
-import { vi } from 'vitest';
-import * as dataService from '../lib/supabaseService';
-import { render, fireEvent } from '@testing-library/svelte'; 
-import dataService.ts from '../src/api/dataService';
-import add from '../src/routes/+page.svelte';
-import App from '../src/routes/+page.svelte';
-import { addTodo } from '../src/api/dataService';
+import { describe, it, expect, vi } from 'vitest';
+import * as dataService from '../api/dataService';
+import { render, fireEvent } from '@testing-library/svelte';
+import TodoPage from '../+page.svelte';
 
-vi.spyOn(dataService, 'addTodo').mockResolvedValue(Promise.resolve());
-vi.spyOn(dataService, 'updateTodo').mockResolvedValue(Promise.resolve());
-
-test('adds a new todo when add fx is called', async () => {
-  const { getByLabelText, getByText } = render(App);  
-  
-  const input = getByLabelText('Add a new to do...'); // fill input
-  await fireEvent.input(input, { target: { value: 'Add a new to do... item'} });
-
-  const button = getByText('Add'); // click the add button
-  await fireEvent.click(button);
-
-  
-});
-
-/* describe('addTodo', () => {
-  it('should correctly handle function calls', async () => {
-    const { data, error } = render(+page.svelte);
-    await fireEvent.click(getByTestId('my-button'));
-
-    expect(dataService.addTodo).toBeCalledTimes(1);
+describe('TodoPage', () => {
+  it('should call addTodo when add is triggered', async () => {
+    const mockTodo = { id: 'uuid', title: 'title', status: false };
+    const addTodoSpy = vi.spyOn(dataService, 'addTodo').mockResolvedValue(mockTodo);
     
+    const { getByText, getByPlaceholderText } = render(TodoPage);
+    const addTodoInput = getByPlaceholderText('Add a new to do...');
+    const addTodoButton = getByText('Add');
+    
+    addTodoInput.value = 'title';
+    await fireEvent.click(addTodoButton);
+
+    expect(addTodoSpy).toBeCalledTimes(1);
+    expect(addTodoSpy).toBeCalledWith(mockTodo);
+    addTodoSpy.mockRestore();
   });
 });
-
-*/
